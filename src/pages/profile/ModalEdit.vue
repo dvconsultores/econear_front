ee1b62415aa712bd2c714c22023379dab1cd5440
detextre4 <detextre4@gmail.com>
2022-07-20 16:11:33 -0500

    profile - wallets + edit

---
+++ src/pages/profile/ModalEdit.vue
@@ -0,0 +1,123 @@
+<template>
+  <v-dialog
+    v-model="modalEdit"
+    max-width="82.358125em"
+  >
+    <v-card color="var(--primary)" class="modalEdit overflowx" style="padding:6em">
+      <v-btn icon class="close" @click="modalEdit = false">
+        <img src="@/assets/icons/close.svg" alt="close" style="--w:0.921875em">
+      </v-btn>
+      <h3 class="tcenter">Edit Profile</h3>
+
+      <v-sheet style="isolation:isolate">
+        <section class="up" style="z-index:-1">
+          <v-btn class="btn" style="--bg:#101C2F;--p:.5em .8em;--bs:0px 2.5px 7px .5px #6FFFE9;width:min(100%, 8.2em)">
+            <span style="font-size: 1.25em">Upload</span>
+          </v-btn>
+          <v-avatar width="8.130625em" height="8.130625em">
+            <img src="@/assets/logos/user.png" alt="avatar" style="--b:3px solid var(--success)">
+          </v-avatar>
+        </section>
+
+        <section class="down fwrap" style="--fb:1 1 clamp(15em, 22vw, 22.76125em)">
+          <div class="divcol">
+            <label for="username">Username</label>
+            <v-text-field
+              v-model="dataModalEdit.username"
+              id="username"
+              solo
+            ></v-text-field>
+          </div>
+
+          <div class="divcol">
+            <label for="email">Email</label>
+            <v-text-field
+              v-model="dataModalEdit.email"
+              id="email"
+              placeholder="example@domain.com"
+              solo
+            ></v-text-field>
+          </div>
+
+          <div class="divcol">
+            <label for="discord_id">Discord ID</label>
+            <v-text-field
+              v-model="dataModalEdit.discord_id"
+              id="discord_id"
+              placeholder="Username#321"
+              solo
+            ></v-text-field>
+          </div>
+
+          <div class="divcol">
+            <label for="website">Website</label>
+            <v-text-field
+              v-model="dataModalEdit.website"
+              id="website"
+              solo
+            ></v-text-field>
+          </div>
+
+          <div class="divcol">
+            <label for="twitter">Twitter Account</label>
+            <v-text-field
+              v-model="dataModalEdit.twitter"
+              id="twitter"
+              solo
+            ></v-text-field>
+          </div>
+
+          <div class="divcol">
+            <label for="discord_server">Discrod Server</label>
+            <v-text-field
+              v-model="dataModalEdit.discord_server"
+              id="discord_server"
+              placeholder="discord.gg/invitecode"
+              solo
+            ></v-text-field>
+          </div>
+
+          <div class="bio divcol">
+            <label for="bio" class="tcenter">Bio</label>
+            <v-textarea
+              v-model="dataModalEdit.bio"
+              id="bio"
+              placeholder="Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+              no-resize
+              solo
+            ></v-textarea>
+          </div>
+
+          <v-btn class="btn align" style="max-width:min(90%,32.3125em);--bs:0 3px 4px 1px hsl(176, 60%, 40%, .3)">
+            <span>Save</span>
+          </v-btn>
+        </section>
+      </v-sheet>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  name: "modalEdit",
+  i18n: require("./i18n"),
+  data() {
+    return {
+      modalEdit: false,
+      dataModalEdit: {
+        username: "",
+        email: "",
+        discord_id: "",
+        website: "",
+        twitter: "",
+        discord_server: "",
+        bio: "",
+      },
+    }
+  },
+  methods: {
+  }
+};
+</script>
+
+<style src="./Profile.scss" lang="scss" />
--- src/pages/profile/Profile.vue
+++ src/pages/profile/Profile.vue
@@ -1,7 +1,11 @@
 <template>
   <section id="profile" class="divcol">
+    <ModalEdit ref="menu"></ModalEdit>
     <aside class="container-header">
-      <v-btn class="btn" style="--bg:#101C2F;--p:.5em .8em;--bs:0px 2.5px 7px .5px #6FFFE9">Edit Profile</v-btn>
+      <v-btn class="btn" style="--bg:#101C2F;--p:.5em .8em;--bs:0px 2.5px 7px .5px #6FFFE9"
+        @click="$refs.menu.modalEdit=true">
+        Edit Profile
+      </v-btn>
     </aside>
 
     <aside class="container-profile divcol center gap1 tcenter">
@@ -14,140 +18,243 @@
           <img :src="require(`@/assets/icons/${item.social}.svg`)" alt="social red" style="--w:2.674375em">
         </v-btn>
       </div>
-      <v-btn class="btn" style="--bg:var(--primary);--bs:0 3px 4px 1px hsl(176, 60%, 40%, 1);--b:1px solid var(--success);width: min(90%,23.8125em);--fs:1.25em">Wallets</v-btn>
-      <p class="p" style="max-width:60ch">
+      <center>
+        <v-btn class="btn" style="--bg:var(--primary);--bs:0 3px 4px 1px hsl(176, 60%, 40%, 1);--b:1px solid var(--success);width: min(90%,23.8125em);--fs:1.25em"
+          @click="switchInfo=!switchInfo">
+          {{switchInfo?'Wallets':'Main info'}}
+        </v-btn>
+      </center>
+      <p v-if="switchInfo==true" class="p" style="max-width:60ch">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut facilisis accumsan nisl, et blandit orci pellentesque
       </p>
     </aside>
 
+    <template v-if="switchInfo">
+      <aside class="space gap2">
+        <h2 class="h9_em p bold">Your Projects Lists</h2>
 
-    <aside class="space gap2">
-      <h2 class="h9_em p bold">Your Projects Lists</h2>
+        <v-text-field
+          v-model="searchProjects"
+          hide-details
+          solo
+          label="Search your projects"
+          append-icon="mdi-magnify"
+          style="max-width:30.061875em;--bg:hsl(210, 48%, 13%, .46);--c:#FFFFFF;--p:0 1.5em"
+          class="search"
+        ></v-text-field>
+      </aside>
 
-      <v-text-field
-        v-model="searchProjects"
-        hide-details
-        solo
-        label="Search your projects"
-        append-icon="mdi-magnify"
-        style="max-width:30.061875em;--bg:hsl(210, 48%, 13%, .46);--c:#FFFFFF;--p:0 1.5em"
-        class="search"
-      ></v-text-field>
-    </aside>
+      <!-- tabla projects  -->
+      <v-data-table
+        class="dataTable card"
+        :headers="headersTableProjects"
+        :items="dataTableProjects"
+        hide-default-footer
+        style="--p:2em"
+      >
+        <template v-slot:[`header.number`]>
+          <span>#</span>
+          <img src="@/assets/icons/sort.svg" alt="sortable icon" style="--w: 0.5em;margin-left:.3em">
+        </template>
 
-    <!-- tabla projects  -->
-    <v-data-table
-      class="dataTable card"
-      :headers="headersTableProjects"
-      :items="dataTableProjects"
-      hide-default-footer
-      style="--p:2em"
-    >
-      <template v-slot:[`header.number`]>
-        <span>#</span>
-        <img src="@/assets/icons/sort.svg" alt="sortable icon" style="--w: 0.5em;margin-left:.3em">
-      </template>
+        <template v-slot:[`item.number`]="{ item }">
+          {{dataTableProjects.indexOf(item) + 1}}
+        </template>
 
-      <template v-slot:[`item.number`]="{ item }">
-        {{dataTableProjects.indexOf(item) + 1}}
-      </template>
+        <template v-slot:[`item.name`]="{ item }">
+          <div class="acenter gap1">
+            <img :src="item.img" alt="nft image" style="--w:4.710625em">
+            <span style="max-width:18ch" class="bold">{{item.name}}</span>
+          </div>
+        </template>
+      </v-data-table>
 
-      <template v-slot:[`item.name`]="{ item }">
-        <div class="acenter gap1">
-          <img :src="item.img" alt="nft image" style="--w:4.710625em">
-          <span style="max-width:18ch" class="bold">{{item.name}}</span>
+
+      <aside class="space gap2">
+        <h2 class="h9_em p bold">Watchlist</h2>
+
+        <v-text-field
+          v-model="searchWatchlist"
+          hide-details
+          solo
+          label="Search your watchlist"
+          append-icon="mdi-magnify"
+          style="max-width:30.061875em;--bg:hsl(210, 48%, 13%, .46);--c:#FFFFFF;--p:0 1.5em"
+          class="search"
+        ></v-text-field>
+      </aside>
+
+      <!-- tabla watchlist  -->
+      <v-data-table
+        class="dataTable card"
+        :headers="headersTableWatchlist"
+        :items="dataTableWatchlist"
+        hide-default-footer
+        style="--p:2em"
+      >
+        <template v-slot:[`header.number`]>
+          <span>#</span>
+          <img src="@/assets/icons/sort.svg" alt="sortable icon" style="--w: 0.5em;margin-left:.3em">
+        </template>
+
+        <template v-slot:[`item.number`]="{ item }">
+          {{dataTableWatchlist.indexOf(item) + 1}}
+        </template>
+
+        <template v-slot:[`item.name`]="{ item }">
+          <div class="acenter gap1">
+            <img :src="item.img" alt="nft image" style="--w:4.710625em">
+            <span style="max-width:18ch" class="bold">{{item.name}}</span>
+          </div>
+        </template>
+      </v-data-table>
+
+
+      <aside class="space gap2">
+        <h2 class="h9_em p bold">Alerts</h2>
+
+        <v-text-field
+          v-model="searchAlerts"
+          hide-details
+          solo
+          label="Search your alerts / NFT Projects name"
+          append-icon="mdi-magnify"
+          style="max-width:30.061875em;--bg:hsl(210, 48%, 13%, .46);--c:#FFFFFF;--p:0 1.5em"
+          class="search"
+        ></v-text-field>
+      </aside>
+
+      <!-- tabla alerts  -->
+      <v-data-table
+        class="dataTable card"
+        :headers="headersTableAlerts"
+        :items="dataTableAlerts"
+        hide-default-footer
+        style="--p:2em"
+      >
+        <template v-slot:[`header.number`]>
+          <span>#</span>
+          <img src="@/assets/icons/sort.svg" alt="sortable icon" style="--w: 0.5em;margin-left:.3em">
+        </template>
+
+        <template v-slot:[`item.number`]="{ item }">
+          {{dataTableAlerts.indexOf(item) + 1}}
+        </template>
+
+        <template v-slot:[`item.name`]="{ item }">
+          <div class="acenter gap1">
+            <img :src="item.img" alt="nft image" style="--w:4.710625em">
+            <span style="max-width:18ch" class="bold">{{item.name}}</span>
+          </div>
+        </template>
+      </v-data-table>
+    </template>
+
+    <section v-else class="container-wallets">
+      <aside class="divcol gap2">
+        <div class="space gap2">
+          <h2 class="h9_em p bold">Your Projects Lists</h2>
+
+          <v-text-field
+            v-model="searchWallets"
+            hide-details
+            solo
+            label="Search your projects"
+            append-icon="mdi-magnify"
+            style="max-width:30.061875em;--bg:hsl(210, 48%, 13%, .46);--c:#FFFFFF;--p:0 1.5em"
+            class="search"
+          ></v-text-field>
         </div>
-      </template>
-    </v-data-table>
 
+        <!-- tabla wallets  -->
+        <v-simple-table
+          class="dataTable wallets card"
+          style="--p:2em"
+        >
+          <template v-slot:default>
+            <thead>
+              <tr>
+                <th class="text-center">
+                  <span>#</span>
+                  <img src="@/assets/icons/sort.svg" alt="sortable icon" style="--w: 0.5em;margin-left:.3em">
+                </th>
+                <th>
+                  Wallet
+                </th>
+                <th class="text-center">
+                  Project Discord
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr
+                v-for="item in dataTableWallets"
+                :key="item.number"
+              >
+                <td class="tcenter">{{dataTableWallets.indexOf(item) + 1}}</td>
+                <td class="tcenter">
+                  <div class="name acenter gap1">
+                    <img :src="item.img" alt="nft image" style="--w:4.710625em">
+                    <v-text-field
+                      v-model="item.name"
+                      placeholder="Wallet name"
+                      type="text"
+                      hide-details
+                    ></v-text-field>
+                    <!-- <span style="max-width:35ch;word-break: break-all" class="bold">{{item.name}}</span> -->
+                  </div>
+                </td>
+                <td class="tcenter">
+                  <v-select
+                    v-model="item.project"
+                    :items="projectList"
+                    :item-value="value"
+                    hide-details
+                    solo
+                    append-icon="mdi-chevron-down"
+                    style="--bg:#21786D"
+                    class="align"
+                  ></v-select>
+                </td>
+              </tr>
 
-    <aside class="space gap2">
-      <h2 class="h9_em p bold">Watchlist</h2>
+              <tr class="add" @click="dataTableWallets.push({
+                  img: require('@/assets/nfts/nft3.png'),
+                  name: '',
+                  project: { value:'All - Default' },
+                })">
+                <td class="tcenter">
+                  <v-icon color="var(--success)">mdi-plus</v-icon>
+                </td>
+                <td>Add</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </template>
+        </v-simple-table>
+      </aside>
 
-      <v-text-field
-        v-model="searchWatchlist"
-        hide-details
-        solo
-        label="Search your watchlist"
-        append-icon="mdi-magnify"
-        style="max-width:30.061875em;--bg:hsl(210, 48%, 13%, .46);--c:#FFFFFF;--p:0 1.5em"
-        class="search"
-      ></v-text-field>
-    </aside>
-
-    <!-- tabla watchlist  -->
-    <v-data-table
-      class="dataTable card"
-      :headers="headersTableWatchlist"
-      :items="dataTableWatchlist"
-      hide-default-footer
-      style="--p:2em"
-    >
-      <template v-slot:[`header.number`]>
-        <span>#</span>
-        <img src="@/assets/icons/sort.svg" alt="sortable icon" style="--w: 0.5em;margin-left:.3em">
-      </template>
-
-      <template v-slot:[`item.number`]="{ item }">
-        {{dataTableWatchlist.indexOf(item) + 1}}
-      </template>
-
-      <template v-slot:[`item.name`]="{ item }">
-        <div class="acenter gap1">
-          <img :src="item.img" alt="nft image" style="--w:4.710625em">
-          <span style="max-width:18ch" class="bold">{{item.name}}</span>
-        </div>
-      </template>
-    </v-data-table>
-
-
-    <aside class="space gap2">
-      <h2 class="h9_em p bold">Alerts</h2>
-
-      <v-text-field
-        v-model="searchAlerts"
-        hide-details
-        solo
-        label="Search your alerts / NFT Projects name"
-        append-icon="mdi-magnify"
-        style="max-width:30.061875em;--bg:hsl(210, 48%, 13%, .46);--c:#FFFFFF;--p:0 1.5em"
-        class="search"
-      ></v-text-field>
-    </aside>
-
-    <!-- tabla watchlist  -->
-    <v-data-table
-      class="dataTable card"
-      :headers="headersTableAlerts"
-      :items="dataTableAlerts"
-      hide-default-footer
-      style="--p:2em"
-    >
-      <template v-slot:[`header.number`]>
-        <span>#</span>
-        <img src="@/assets/icons/sort.svg" alt="sortable icon" style="--w: 0.5em;margin-left:.3em">
-      </template>
-
-      <template v-slot:[`item.number`]="{ item }">
-        {{dataTableAlerts.indexOf(item) + 1}}
-      </template>
-
-      <template v-slot:[`item.name`]="{ item }">
-        <div class="acenter gap1">
-          <img :src="item.img" alt="nft image" style="--w:4.710625em">
-          <span style="max-width:18ch" class="bold">{{item.name}}</span>
-        </div>
-      </template>
-    </v-data-table>
+      <article class="notes card">
+        <h4>Note</h4>
+        <p>
+          The wallet address put forward to projects for whitelisting is the address that is configured as "All - Default". 
+          This can be changed as many times up until the project finisches whitelisting and has fully exported their whitelist addresses.
+        </p>
+        <p class="p">You can link a wallet address to any project on your whitelist</p>
+      </article>
+    </section>
   </section>
 </template>
 
 <script>
+import ModalEdit from './ModalEdit.vue'
 export default {
   name: "profile",
   i18n: require("./i18n"),
+  components: { ModalEdit },
   data() {
     return {
+      switchInfo: true,
       dataSocialRed: [
         { social: "clip", link: "#" },
         { social: "twitter", link: "#" },
@@ -247,6 +354,28 @@ export default {
           discord_server: "discord.gg/invitecode",
         },
       ],
+      // wallets
+      searchWallets: "",
+      dataTableWallets: [
+        {
+          img: require("@/assets/nfts/nft1.png"),
+          name: "pedrogperez23.near",
+          project: { value:"All - Default" },
+        },
+        {
+          img: require("@/assets/nfts/nft2.png"),
+          name: "609e3c23a999964d76833hbjj5.264f11e10",
+          project: { value:"All - Default" },
+        },
+      ],
+      projectList: [
+        "All - Default",
+        "Discord Server Project Name",
+        "Discord Server Project Name2",
+        "Discord Server Project Name3",
+        "Discord Server Project Name4",
+        "Discord Server Project Name5",
+      ]
     }
   },
   methods: {
